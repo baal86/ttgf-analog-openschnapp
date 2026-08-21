50f64c6b1682b4b5cac8d5218f19c7a58e7d878b
Mathias Richerzhagen <mricherz@eso.org>
2026-08-21 16:25:21 +0200

    LVS kind of working. Fixing property mismatch!

--- mag/tcl/lvs_netgen.tcl
+++ mag/tcl/lvs_netgen.tcl
@@ -1,7 +1,7 @@
 set layout [readnet spice $project.lvs.spice]
 set source [readnet spice /dev/null]
 #readnet spice $::env(PDK_ROOT)/$::env(PDK)/libs.tech/ngspice/sm141064.spice $layout
-readnet spice $::env(PDK_ROOT)/$::env(PDK)/libs.tech/ngspice/sm141064.spice $source
+#readnet spice $::env(PDK_ROOT)/$::env(PDK)/libs.tech/ngspice/sm141064.spice $source
 
 # top level GL verilog
 readnet verilog ../src/project.v $source
