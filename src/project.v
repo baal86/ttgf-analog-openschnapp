481f655e07ace329e8e16ecb8898e177993d0845
baal86 <43853528+baal86@users.noreply.github.com>
2026-09-01 08:53:01 +0200

    Second supply rail not required.

--- src/project.v
+++ src/project.v
@@ -8,7 +8,7 @@
 module tt_um_baal86_openschnapp (
     input  wire       VGND,
     input  wire       VDPWR,    // 3.3v core power supply
-    input  wire       VAPWR,    // second analog power supply (VAA)
+    //input  wire       VAPWR,    // second analog power supply (VAA)
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
