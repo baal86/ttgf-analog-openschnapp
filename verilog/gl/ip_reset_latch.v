6a5f447c1327daee80d65c91b1da299a523e7f3f
baal86 <43853528+baal86@users.noreply.github.com>
2026-09-04 09:29:00 +0200

    Added reset latch

---
+++ verilog/gl/ip_reset_latch.v
@@ -0,0 +1,167 @@
+module ip_reset_latch (A,
+    Y,
+    clk,
+    VDD,
+    VSS);
+ input A;
+ output Y;
+ input clk;
+ inout VDD;
+ inout VSS;
+
+ wire net1;
+ wire net3;
+ wire net2;
+
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_8 FILLER_0_18 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_16 FILLER_0_2 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fill_2 FILLER_0_32 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_8 FILLER_0_36 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_4 FILLER_0_44 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fill_1 FILLER_0_48 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_32 FILLER_1_2 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_8 FILLER_1_34 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_4 FILLER_1_42 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fill_2 FILLER_1_46 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fill_1 FILLER_1_48 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_32 FILLER_2_2 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fill_1 FILLER_2_34 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_8 FILLER_2_37 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_4 FILLER_2_45 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_4 FILLER_3_2 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_8 FILLER_3_35 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_4 FILLER_3_43 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fill_2 FILLER_3_47 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fill_2 FILLER_4_10 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_16 FILLER_4_18 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_8 FILLER_4_2 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fill_2 FILLER_4_36 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fillcap_4 FILLER_4_44 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__fill_1 FILLER_4_48 (.VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__endcap PHY_EDGE_ROW_0_Left_5 (.VDD(VDD),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__endcap PHY_EDGE_ROW_0_Right_0 (.VDD(VDD),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__endcap PHY_EDGE_ROW_1_Left_6 (.VDD(VDD),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__endcap PHY_EDGE_ROW_1_Right_1 (.VDD(VDD),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__endcap PHY_EDGE_ROW_2_Left_7 (.VDD(VDD),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__endcap PHY_EDGE_ROW_2_Right_2 (.VDD(VDD),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__endcap PHY_EDGE_ROW_3_Left_8 (.VDD(VDD),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__endcap PHY_EDGE_ROW_3_Right_3 (.VDD(VDD),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__endcap PHY_EDGE_ROW_4_Left_9 (.VDD(VDD),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__endcap PHY_EDGE_ROW_4_Right_4 (.VDD(VDD),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__filltie TAP_TAPCELL_ROW_0_10 (.VDD(VDD),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__filltie TAP_TAPCELL_ROW_2_11 (.VDD(VDD),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__filltie TAP_TAPCELL_ROW_4_12 (.VDD(VDD),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__dffq_1 _0_ (.D(net1),
+    .CLK(net2),
+    .Q(net3),
+    .VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__buf_1 input1 (.I(A),
+    .Z(net1),
+    .VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__buf_1 input2 (.I(clk),
+    .Z(net2),
+    .VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+ gf180mcu_fd_sc_mcu7t5v0__buf_1 output3 (.I(net3),
+    .Z(Y),
+    .VDD(VDD),
+    .VNW(VDD),
+    .VPW(VSS),
+    .VSS(VSS));
+endmodule
