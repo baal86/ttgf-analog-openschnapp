3ac1c5fd86fa105bd37e75c8c3714f1a1749855b
baal86 <43853528+baal86@users.noreply.github.com>
2026-09-09 17:06:43 +0200

    Prettier rendering of figure

--- xschem/test/tb_current_source_dc.py
+++ xschem/test/tb_current_source_dc.py
@@ -2,10 +2,16 @@ import pytest
 from spicelib import RawRead
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.lines import Line2D
 
-MIN_DC = 0.5
-MIN_CURRENT = 0.8 * 1.0e-6
-MAX_CURRENT = 1.2 * 1.0e-6
+MIN_VOLTAGE = 0.5
+MIN_CURRENT = 0.8
+MAX_CURRENT = 1.2
+
+CURRENTS = ["i(vimir)","i(vimir1)","i(vimir_parax)","i(vimir_parax1)"]
+COLORS   = ["aqua","blue", "teal", "navy"]
+
+plt.rcParams['lines.linewidth'] = 0.5
 
 @pytest.fixture
 def data():
@@ -16,14 +22,15 @@ def data():
 def steps(data):
     return range(len(data.plots))
 
-@pytest.mark.parametrize("trace",["i(vimir)","i(vimir1)"])
+
+@pytest.mark.parametrize("trace",CURRENTS)
 def test_minmax_current(data,steps,trace):
     for i in steps:
         plot = data.plots[i]
         axis = plot.get_trace(0).get_wave()
         current = plot.get_trace(trace).get_wave()
 
-        mask = axis > MIN_DC
+        mask = axis > MIN_VOLTAGE
 
         ok = np.all(current[mask] >= MIN_CURRENT)
         assert ok, f"Minimum current not met for {trace}, step {i}"
@@ -34,14 +41,20 @@ def test_minmax_current(data,steps,trace):
 
 def test_plot_current(data,steps):
     plt.figure()
-    for i in steps:
-        current_plot = data.plots[i]
-        axis = current_plot.get_trace(0).get_wave()
-        current = current_plot.get_trace("i(vimir)").get_wave()
-        plt.plot(axis,current,"b")
-        current = current_plot.get_trace("i(vimir1)").get_wave()
-        plt.plot(axis,current,"g")
-    plt.hlines(MIN_CURRENT,MIN_DC,3.3,"r")
+    for c,col in zip(CURRENTS,COLORS):
+        for i in steps:
+            current_plot = data.plots[i]
+            axis = current_plot.get_trace(0).get_wave()
+            current = current_plot.get_trace(c).get_wave()
+            plt.plot(axis,current,col)
+    plt.hlines(MIN_CURRENT,MIN_VOLTAGE,3.3,"r")
     plt.hlines(MAX_CURRENT,0.0,3.3,"r")
 
+    plt.grid(which="both")  
+    handles = [
+        Line2D([0], [0], color=color, lw=2, label=current)
+        for current, color in zip(CURRENTS, COLORS)
+    ]
+
+    plt.legend(handles=handles)
     plt.savefig("tb_current_source_dc.png")
