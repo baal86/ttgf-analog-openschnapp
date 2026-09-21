f6ef57f699231d6941f3e8b5e3202e81a693b2fd
baal86 <43853528+baal86@users.noreply.github.com>
2026-09-21 16:33:06 +0200

    another large cap in top left

--- src/project.v
+++ src/project.v
@@ -87,6 +87,7 @@ module tt_um_baal86_openschnapp (
     ip_psucap_l psucapl_16(.VDD(VDPWR),.VSUBS(VGND));
 
     ip_psucap_br psucapl_br(.VDD(VDPWR),.VSUBS(VGND));
+    ip_psucap_br psucapl_tl(.VDD(VDPWR),.VSUBS(VGND));
 
     ip_pixel_nd2ps_dummy pixel_dummy(
         .VDD(VDPWR), 
