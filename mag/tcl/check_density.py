10fcaf7befce7ce4c71c93ccb468161553d35191
baal86 <43853528+baal86@users.noreply.github.com>
2026-09-23 14:32:14 +0200

    Keepout Grid on M1 pixels

--- mag/tcl/check_density.py
+++ mag/tcl/check_density.py
@@ -5,6 +5,6 @@ top = ly.top_cell()
 die = top.dbbox().area()
 
 # (name, layer, datatype): check against your PDK layer map
-for name, l, d in [("M1", 34, 0), ("M2", 36, 0), ("M3", 42, 0), ("M4", 46, 0), ("M5", 81, 0), ("Poly2", 30, 0)]:
+for name, l, d in [("M1", 34, 0), ("M2", 36, 0), ("M3", 42, 0), ("M4", 46, 0), ("M5", 81, 0), ("Poly2", 30, 0), ("COMP", 22, 0)]:
     r = pya.Region(top.begin_shapes_rec(ly.layer(l, d))).merged()
-    print(f"{name}: {100 * r.area() * ly.dbu**2 / die:.2f} %")
+    print(f"{name}:\t{100 * r.area() * ly.dbu**2 / die:.2f} %")
